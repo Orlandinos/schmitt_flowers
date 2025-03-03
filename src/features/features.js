dc81dd714a8c9c6d46b743a669d4aa8d3116e5de
Orlandinos <casperx89@gmail.com>
2025-03-03 10:14:36 +0100

    Start Prodaction

---
+++ src/features/features.js
@@ -0,0 +1,19 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+
+
+
+
+
+
+const counterSlice = createSlice({
+    name: 'auth',
+    initialState: { value: 0 },           //потом заменить!
+    
+    reducers: {
+
+    }
+
+});
+
+export default counterSlice.reducer;
